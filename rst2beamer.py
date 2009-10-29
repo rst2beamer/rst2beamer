f8a5dc4c920a1dd3ed7968eb6bfeabbcb0b51a0e
Paul-Michael Agapow <github@agapow.net>
2009-10-29 13:40:38 +0000

    Possible hack for enumerated lists with docutils 0.6

--- rst2beamer.py
+++ rst2beamer.py
@@ -668,7 +668,8 @@ class BeamerTranslator (LaTeXTranslator):
         #visit_enumerated_list that throws out much of what latex
         #does to handle them for us.  I am going back to relying
         #on latex.
-        if 'contents' in self.topic_classes:
+        if (hasattr (self, 'topic_classes') and
+                ('contents' in self.topic_classes)):
             if self.use_latex_toc:
                 raise nodes.SkipNode
             self.body.append( '\\begin{list}{}{}\n' )
@@ -685,70 +686,12 @@ class BeamerTranslator (LaTeXTranslator):
 
 
     def depart_enumerated_list (self, node):
-        if 'contents' in self.topic_classes:
+        if (hasattr (self, 'topic_classes') and
+                ('contents' in self.topic_classes)):
             self.body.append( '\\end{list}\n' )
         else:
             self.body.append( '\\end{enumerate}\n' )
 
-
-    def visit_image (self, node):
-        if self.centerfigs:
-            self.body.append('\\begin{center}\n')
-        attrs = node.attributes
-        # Add image URI to dependency list, assuming that it's
-        # referring to a local file.
-        self.settings.record_dependencies.add(attrs['uri'])
-        pre = []                        # in reverse order
-        post = []
-        include_graphics_options = []
-        inline = isinstance(node.parent, nodes.TextElement)
-        if 'scale' in attrs:
-            # Could also be done with ``scale`` option to
-            # ``\includegraphics``; doing it this way for consistency.
-            pre.append('\\scalebox{%f}{' % (attrs['scale'] / 100.0,))
-            post.append('}')
-        if 'width' in attrs:
-            include_graphics_options.append('width=%s' % (
-                            self.latex_image_length(attrs['width']), ))
-        if 'height' in attrs:
-            include_graphics_options.append('height=%s' % (
-                            self.latex_image_length(attrs['height']), ))
-        if ('height' not in attrs) and ('width' not in attrs):
-            include_graphics_options.append('height=0.75\\textheight')
-
-        if 'align' in attrs:
-            align_prepost = {
-                # By default latex aligns the bottom of an image.
-                (1, 'bottom'): ('', ''),
-                (1, 'middle'): ('\\raisebox{-0.5\\height}{', '}'),
-                (1, 'top'): ('\\raisebox{-\\height}{', '}'),
-                (0, 'center'): ('{\\hfill', '\\hfill}'),
-                # These 2 don't exactly do the right thing.  The image should
-                # be floated alongside the paragraph.  See
-                # http://www.w3.org/TR/html4/struct/objects.html#adef-align-IMG
-                (0, 'left'): ('{', '\\hfill}'),
-                (0, 'right'): ('{\\hfill', '}'),}
-            try:
-                pre.append(align_prepost[inline, attrs['align']][0])
-                post.append(align_prepost[inline, attrs['align']][1])
-            except KeyError:
-                pass                    # XXX complain here?
-        if not inline:
-            pre.append('\n')
-            post.append('\n')
-        pre.reverse()
-        self.body.extend( pre )
-        options = ''
-        if len(include_graphics_options)>0:
-            options = '[%s]' % (','.join(include_graphics_options))
-        self.body.append( '\\includegraphics%s{%s}' % (
-                            options, attrs['uri'] ) )
-        self.body.extend( post )
-
-
-    def depart_image (self, node):
-        if self.centerfigs:
-            self.body.append('\\end{center}\n')
 
 
     def astext (self):
